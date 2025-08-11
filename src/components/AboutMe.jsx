4ae7d79767978f882ad445b471908911ab6b3036
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-11 19:55:26 +0530

    animation on about me page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { AnimatedBtn } from './AnimatedLink'
+import { BoldText, UnderlineHighlight, Highlight} from './TransitionOverlay';
 
-function About() {
+
+export default function About() {
 
   return (
     <div className="w-full h-screen">
@@ -9,21 +11,11 @@ function About() {
 
       <div className="w-[80%] h-auto absolute -translate-x-1/2 left-1/2 top-0">
         <div className="w-full min-h-screen flex justify-center items-center">
-          <h1 className="text-6xl leading-25">
-            Need a website that looks great and performs perfectly?
-            That takes a blend of design sense, clean code, and attention to detail —
-            which is exactly what I bring as a front-end developer crafting responsive, modern UI's.
-          </h1>
-        </div>
-
-        <div className="w-full min-h-screen flex justify-center items-center">
-          <h1 className="text-6xl leading-25">
-            Hello, my name is Rajendra Behera, a front-end developer seeking an internship or job where I can put my skills into action on real-world projects and continue growing as a developer. While I currently specialize in front-end development, I plan to learn backend technologies so I can apply that knowledge in future projects and contribute more broadly to the team.
+          <h1 className="text-5xl leading-22">
+            Hello, my name is <BoldText>Rajendra Behera</BoldText>, I am a front-end developer seeking an <Highlight>internship</Highlight> or job where I can put my skills into action on real-world projects and continue growing as a developer. While I currently specialize in front-end development, I plan to learn <UnderlineHighlight>backend technologies</UnderlineHighlight>  so I can apply that knowledge in future projects and contribute more broadly to the team.
           </h1>
         </div>
       </div>
     </div>
   )
 }
-
-export default About
--- src/components/TransitionOverlay.jsx
+++ src/components/TransitionOverlay.jsx
@@ -37,3 +37,50 @@ export default function TransitionOverlay({ clickPosition }) {
     />
   );
 }
+
+export function Highlight({ children }) {
+  return (
+    <span className="relative inline-block">
+      <motion.span
+        className="absolute inset-0 z-0 bg-yellow-300 rounded-lg"
+        style={{
+          originX: 0,
+          clipPath: 'polygon(0% 20%, 100% 0%, 100% 80%, 0% 100%)', // makes it uneven
+        }}
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: 1 }}
+        transition={{ duration: 0.6, ease: 'easeOut' }}
+      />
+      <span className="relative px-2 py-0 z-10 text-stone-50 font-semibold">{children}</span>
+    </span>
+  );
+}
+
+export function BoldText({ children }) {
+
+  return (
+    <motion.span
+      initial={{ fontVariationSettings: '"wght" 400' }}
+      animate={{ fontVariationSettings: '"wght" 700' }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.span>
+  );
+}
+
+export function UnderlineHighlight({ children, duration = 0.6, delay = 0 }) {
+  return (
+    <span className="relative inline-block whitespace-nowrap"> 
+      <span className="relative z-10">{children}</span>
+
+      <motion.span
+        className="absolute left-0 right-0 bottom-1 h-[5px] bg-yellow-300 rounded-sm pointer-events-none z-0"
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: 1 }}
+        transition={{ duration, ease: "easeOut", delay }}
+        style={{ transformOrigin: "0 50%" }}
+      />
+    </span>
+  );
+}
