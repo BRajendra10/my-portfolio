9d1dad31b6acb09c47c64c1d81d1381c49ca8717
BRajendra10 <rajendrabehera8116@gmail.com>
2025-09-10 07:33:32 +0530

    setup my portflio repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-thin">Welcome to my portfolio</h1>
+    </div>
+  )
+}
+
+export default App
